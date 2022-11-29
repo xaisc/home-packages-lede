9d512720ca65c55ce006baeb2f8ae51040eb4505
xaisc <net_tech@sina.com>
2022-11-29 14:52:40 +0800

    add

--- main.sh
+++ main.sh
@@ -159,6 +159,7 @@ svn export https://github.com/haiibo/openwrt-packages/trunk/udp2raw
 svn export https://github.com/kuoruan/openwrt-upx/trunk/upx
 svn export https://github.com/kuoruan/openwrt-upx/trunk/ucl
 svn export https://github.com/immortalwrt/packages/trunk/libs/libcryptopp
+svn export https://github.com/immortalwrt/packages/trunk/devel/go-rice
 
 git_sparse_clone master "https://github.com/coolsnowwolf/packages" "leanpack" net/miniupnpd net/mwan3 multimedia/UnblockNeteaseMusic-Go \
 multimedia/UnblockNeteaseMusic net/amule net/baidupcs-web multimedia/gmediarender net/go-aliyundrive-webdav \
